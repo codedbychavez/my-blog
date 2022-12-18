8b335b3a1c93f827459439c8e3b99841d64aa8dc
Chavez Harris <codedbychavez@gmail>
2022-12-18 18:34:17 -0300

    Update

--- .nuxt/app.config.mjs
+++
@@ -1,8 +0,0 @@
-
-import { defuFn } from '/home/codedbychavez/repos/codedbychavez.com/node_modules/defu/dist/defu.mjs'
-
-const inlineConfig = {}
-
-
-
-export default defuFn(inlineConfig)
--- README.md
+++ README.md
@@ -1,3 +1,1 @@
-test
-
-dddd
+# codedbychavez.com
