683d5112c52f196af8e506a7a9f66c8ddb759315
Chavez Harris <codedbychavez@gmail>
2022-12-18 18:30:13 -0300

    Updated SEO

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      charset: 'utf-16',
+      viewport: 'initial-scale=1',
+      title: 'codedbychavez',
+      meta: [
+        // <meta name="description" content="My amazing site">
+        { name: 'description', content: 'Learn CSS and HTML like never before while having fun along the way.' }
+      ],
+    }
+  },
   modules: ['@nuxtjs/tailwindcss'],
   css: [
     '~/assets/css/main.scss'
